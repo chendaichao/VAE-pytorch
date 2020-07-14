86ed94c416ef5f499b97a28ef908a5a26d1612ab
Daichao Chen <67040079+chendaichao@users.noreply.github.com>
2020-07-14 15:42:20 +0800

    Add files via upload

---
+++ Models/VAE/model.py
@@ -0,0 +1,128 @@
+import torch
+import numpy as np
+import torch.nn as nn
+from collections import OrderedDict
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+    def forward(self, x):
+        batch_size = x.shape[0]
+        return x.view(batch_size, -1)
+
+class MLP(nn.Module):
+    def __init__(self, hidden_size, last_activation = True):
+        super(MLP, self).__init__()
+        q = []
+        for i in range(len(hidden_size)-1):
+            in_dim = hidden_size[i]
+            out_dim = hidden_size[i+1]
+            q.append(("Linear_%d" % i, nn.Linear(in_dim, out_dim)))
+            if (i < len(hidden_size)-2) or ((i == len(hidden_size) - 2) and (last_activation)):
+                q.append(("BatchNorm_%d" % i, nn.BatchNorm1d(out_dim)))
+                q.append(("ReLU_%d" % i, nn.ReLU(inplace=True)))
+        self.mlp = nn.Sequential(OrderedDict(q))
+    def forward(self, x):
+        return self.mlp(x)
+    
+class Encoder(nn.Module):
+    def __init__(self, shape, nhid = 16, ncond = 0):
+        super(Encoder, self).__init__()
+        c, w, h = shape
+        self.encode = nn.Sequential(nn.Conv2d(1, 8, 7, padding = 3), nn.BatchNorm2d(8), nn.ReLU(inplace = True), 
+                                    nn.Conv2d(8, 16, 7, padding = 3), nn.BatchNorm2d(16), nn.ReLU(inplace = True), 
+                                    nn.MaxPool2d(2, 2),
+                                    nn.Conv2d(16, 16, 5, padding = 2), nn.BatchNorm2d(16), nn.ReLU(inplace = True), 
+                                    nn.Conv2d(16, 32, 5, padding = 2), nn.BatchNorm2d(32), nn.ReLU(inplace = True), 
+                                    nn.MaxPool2d(2, 2),
+                                    Flatten(), MLP([w*h*2, 256, 128])
+                                   )
+        self.calc_mean = MLP([128+ncond, 64, nhid], last_activation = False)
+        self.calc_logvar = MLP([128+ncond, 64, nhid], last_activation = False)
+    def forward(self, x, y = None):
+        x = self.encode(x)
+        if (y is None):
+            return self.calc_mean(x), self.calc_logvar(x)
+        else:
+            return self.calc_mean(torch.cat((x, y), dim=1)), self.calc_logvar(torch.cat((x, y), dim=1))
+
+class Decoder(nn.Module):
+    def __init__(self, shape, nhid = 16, ncond = 0):
+        super(Decoder, self).__init__()
+        c, w, h = shape
+        self.shape = shape
+        self.decode = nn.Sequential(MLP([nhid+ncond, 128, 256, c*w*h], last_activation = False), nn.Sigmoid())
+    def forward(self, z, y = None):
+        c, w, h = self.shape
+        if (y is None):
+            return self.decode(z).view(-1, c, w, h)
+        else:
+            return self.decode(torch.cat((z, y), dim=1)).view(-1, c, w, h)
+
+class VAE(nn.Module):
+    def __init__(self, shape, nhid = 16):
+        super(VAE, self).__init__()
+        self.dim = nhid
+        self.encoder = Encoder(shape, nhid)
+        self.decoder = Decoder(shape, nhid)
+        
+    def sampling(self, mean, logvar):
+        eps = torch.randn(mean.shape).to(device)
+        sigma = 0.5 * torch.exp(logvar)
+        return mean + eps * sigma
+    
+    def forward(self, x):
+        mean, logvar = self.encoder(x)
+        z = self.sampling(mean, logvar)
+        return self.decoder(z), mean, logvar
+    
+    def generate(self, batch_size = None):
+        z = torch.randn((batch_size, self.dim)).to(device) if batch_size else torch.randn((1, self.dim)).to(device)
+        res = self.decoder(z)
+        if not batch_size:
+            res = res.squeeze(0)
+        return res
+
+class cVAE(nn.Module):
+    def __init__(self, shape, nclass, nhid = 16):
+        super(cVAE, self).__init__()
+        self.dim = nhid
+        self.encoder = Encoder(shape, nhid, ncond = nhid)
+        self.decoder = Decoder(shape, nhid, ncond = nhid)
+        self.label_embedding = nn.Embedding(nclass, nhid)
+        
+    def sampling(self, mean, logvar):
+        eps = torch.randn(mean.shape).to(device)
+        sigma = 0.5 * torch.exp(logvar)
+        return mean + eps * sigma
+    
+    def forward(self, x, y):
+        y = self.label_embedding(y)
+        mean, logvar = self.encoder(x, y)
+        z = self.sampling(mean, logvar)
+        return self.decoder(z, y), mean, logvar
+    
+    def generate(self, class_idx):
+        if (type(class_idx) is int):
+            class_idx = torch.tensor(class_idx)
+        class_idx = class_idx.to(device)
+        if (len(class_idx.shape) == 0):
+            batch_size = None
+            class_idx = class_idx.unsqueeze(0)
+            z = torch.randn((1, self.dim)).to(device)
+        else:
+            batch_size = class_idx.shape[0]
+            z = torch.randn((batch_size, self.dim)).to(device) 
+        y = self.label_embedding(class_idx)
+        res = self.decoder(z, y)
+        if not batch_size:
+            res = res.squeeze(0)
+        return res
+    
+BCE_loss = nn.BCELoss(reduction = "sum")
+def loss(X, X_hat, mean, logvar):
+    reconstruction_loss = BCE_loss(X_hat, X)
+    KL_divergence = 0.5 * torch.sum(-1 - logvar + torch.exp(logvar) + mean**2)
+    return reconstruction_loss + KL_divergence
---
+++ Models/VAE/train_VAE.py
@@ -0,0 +1,83 @@
+import torch
+import numpy as np
+import torch.nn.functional as F
+import torchvision
+import os, time, tqdm
+from model import loss, VAE
+from utils import EarlyStop
+
+############## loading data ###################
+
+def crop(x, low, high):
+    x[x<=low] = low
+    x[x>=high] = high
+    return x
+
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Lambda(lambda x: crop(x, 0., 1.))
+])
+
+train_data = torchvision.datasets.MNIST(root='../../Datasets', train=True, download=True, transform=transform)
+train_iter = torch.utils.data.DataLoader(train_data, batch_size=128, shuffle=True)
+
+############## loading models ###################
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+net = VAE((1, 28, 28), nhid = 16)
+net.to(device)
+print(net)
+save_name = "VAE.pt"
+
+############### training #########################
+
+lr = 0.03
+optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, weight_decay = 0.0001)
+
+def adjust_lr(optimizer, decay_rate=0.95):
+    for param_group in optimizer.param_groups:
+        param_group['lr'] *= decay_rate
+
+retrain = True
+if os.path.exists(save_name):
+    print("Model parameters have already been trained before. Retrain ? (y/n)")
+    ans = input()
+    if not (ans == 'y'):
+        checkpoint = torch.load(save_name, map_location = device)
+        net.load_state_dict(checkpoint["net"])
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        for g in optimizer.param_groups:
+            g['lr'] = lr
+        
+max_epochs = 1000
+early_stop = EarlyStop(patience = 20, save_name = save_name)
+net = net.to(device)
+
+print("training on ", device)
+for epoch in range(max_epochs):
+
+    train_loss, n, start = 0.0, 0, time.time()
+    for X, _ in tqdm.tqdm(train_iter, ncols = 50):
+        X = X.to(device)
+        X_hat, mean, logvar = net(X)
+
+        l = loss(X, X_hat, mean, logvar).to(device)
+        optimizer.zero_grad()
+        l.backward()
+        optimizer.step()
+
+        train_loss += l.cpu().item()
+        n += X.shape[0]
+
+    train_loss /= n
+    print('epoch %d, train loss %.4f , time %.1f sec'
+          % (epoch, train_loss, time.time() - start))
+    
+    adjust_lr(optimizer)
+    
+    if (early_stop(train_loss, net, optimizer)):
+        break
+
+checkpoint = torch.load(early_stop.save_name)
+net.load_state_dict(checkpoint["net"])
